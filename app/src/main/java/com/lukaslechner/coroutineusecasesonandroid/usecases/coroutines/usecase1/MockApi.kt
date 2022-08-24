5abfa41619d113188ffe854298c7d855e8096ab7
Lucas Ibiapina <66168574+lucas-ibiapina-silva@users.noreply.github.com>
2022-08-24 09:30:13 -0300

    Section 4:21-25 launch and runBlocking

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase1/MockApi.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase1/MockApi.kt
@@ -10,8 +10,9 @@ fun mockApi() =
         MockNetworkInterceptor()
             .mock(
                 "http://localhost/recent-android-versions",
-                Gson().toJson(mockAndroidVersions),
-                200,
+                body = "Something went wrong on server side",
+                //Gson().toJson(mockAndroidVersions),
+                500,
                 1500
             )
     )
