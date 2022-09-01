791e7f3358c9fa9505ada175501c502bd790ae40
Lucas Ibiapina <66168574+lucas-ibiapina-silva@users.noreply.github.com>
2022-09-01 14:39:12 -0300

    Section 6 - retries and exponetial backoff

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase6/RetryNetworkRequestViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase6/RetryNetworkRequestViewModel.kt
@@ -3,6 +3,7 @@ package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase6
 import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
 import com.lukaslechner.coroutineusecasesonandroid.mock.MockApi
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import timber.log.Timber
 
@@ -16,22 +17,35 @@ class RetryNetworkRequestViewModel(
         viewModelScope.launch {
             val numberOfTries = 2
             try {
-                repeat(numberOfTries) {
-                    try {
-                        loadRecentAndroidVersions()
-                        return@launch
-                    }
-                    catch (ex: Exception) {
-                        Timber.e(ex)
-                    }
+                retry(numberOfTries) {
+                    loadRecentAndroidVersions()
                 }
-                loadRecentAndroidVersions()
             }
             catch (e: Exception) {
                 Timber.e(e)
                 uiState.value = UiState.Error("Network request failed!")
             }
         }
+    }
+
+    private suspend fun <T> retry(
+        numberOfRetries: Int,
+        initialDelayMillis: Long = 100,
+        maxDelayMillis: Long = 1000,
+        factor: Double = 2.0,
+        block: suspend () -> T
+    ) : T {
+        var currentDelay = initialDelayMillis
+        repeat(numberOfRetries) {
+            try {
+                return block()
+            } catch (ex: Exception) {
+                Timber.e(ex)
+            }
+            delay(currentDelay)
+            currentDelay = (currentDelay * factor).toLong().coerceAtMost(maxDelayMillis)
+        }
+        return block()
     }
 
     private suspend fun loadRecentAndroidVersions() {
